e5cc12a720d17ce05b519dc88237d1560b7bb2a9
Ruofeng Tang <trf873@uw.edu>
2021-06-01 15:30:03 +0800

    add a test

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -6,3 +6,8 @@ test_that("my_t.test works mathematically", {
   expect_equal(as.numeric(my_t.test(my_gapminder$lifeExp, "greater", 60)[4]),
                0.95338562)
 })
+
+test_that("my_t.test stops if test type is incorrect", {
+  expect_error(my_t.test(my_gapminder$lifeExp, "what", 60),
+                 'wrong input. Try "two.sided", "less", or "greater".')
+})
